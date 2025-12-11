390e6ef9aa2b635c8edfd4d9209fca16937eb1f7
Rudy Eko Prasetya <rudyekoprasetya@yahoo.co.id>
2025-12-11 13:54:59 +0700

    [ADD] proses trasnformasi data

--- submission/utils/transform.py
+++ submission/utils/transform.py
@@ -11,20 +11,23 @@ def transform_data(df):
         df_transformed = df_transformed.assign(
             Title=df_transformed['Title'].fillna('Unknown Product'),
             Rating=df_transformed['Rating'].fillna('Invalid Rating'),
-            Price=df_transformed['Price'].fillna('Price Unavailable'),
+            Price=df_transformed['Price'].fillna(0),
             Size=df_transformed['Size'].fillna('Not Specified')
         )
 
         #transformasi untuk kolom price
+        df_transformed['Price'] = df_transformed['Price'].replace('Price Unavailable',0)
         df_transformed['Price'] = df_transformed['Price'].astype(np.float64) # konversi ke float64
         df_transformed.loc[df_transformed['Price'] < 0, 'Price'] = 0 # ubah nilai negatif ke 0
         df_transformed['Price'] = df_transformed['Price']*16000 # jadikan rupiah
 
         # transformasi kolom rating
+        df_transformed['Rating'] = df_transformed['Rating'].replace('Invalid Rating',0)
+        df_transformed['Rating'] = df_transformed['Rating'].replace('Not Rated',0)
         df_transformed['Rating'] = df_transformed['Rating'].round().astype(np.int64)
 
         # transformasi untuk kolom Colors
-        df_transformed['Colors'] = df_transformed['Colors'].astype(np.float64)
+        df_transformed['Colors'] = df_transformed['Colors'].astype(np.int64)
 
         return df_transformed
 
